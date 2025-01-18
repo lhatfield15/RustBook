3e929432a9a4716ebb614c6728f5364d7c092162
Lauren Hatfield <lhatfield15@gmail.com>
2025-01-18 14:47:07 -0500

    Pretty print

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,9 +1,16 @@
-fn main() {
-    let rect = (30, 50);
-
-    println!("Area is {} pixels.", area(rect));
+#[derive(Debug)]
+struct Rectangle {
+    w: u32,
+    h: u32,
 }
 
-fn area(dims: (u32, u32)) -> u32 {
-    dims.0 * dims.1
+fn main() {
+    let rect = Rectangle { w: 30, h: 50 };
+
+    println!("Area is {} pixels.", area(&rect));
+    println!("Rect is {rect:#?}")
+}
+
+fn area(rect: &Rectangle) -> u32 {
+    rect.h * rect.w
 }
